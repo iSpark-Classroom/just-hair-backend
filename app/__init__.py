8722ec2f84b2c636eefc9e5efe4b9e18c6bd80f9
marivonashienji@gmail.com <marivon-ashie>
2025-12-05 13:18:13 +0100

    - create a new file called reviews  for the endpoints for the reviews table

--- app/__init__.py
+++ app/__init__.py
@@ -1,63 +1,49 @@
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager
-from flask_cors import CORS
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
-
-
-limiter = Limiter(key_func=get_remote_address, default_limits=["500 per day", "100 per hour"])  # no app yet
+from app.config import Config
+import os
+from flask_swagger_ui import get_swaggerui_blueprint
 
 db = SQLAlchemy()
 migrate = Migrate()
-bcrypt = Bcrypt()
-jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object("app.config.Config")
-
-    # initialize extensions
-    db.init_app(app)
-    migrate.init_app(app, db)
-    bcrypt.init_app(app)
-    jwt.init_app(app)
-    limiter.init_app(app)
-
-    # CORS(app, resources={r"/*": {"origins": "*"}})
-    CORS(
-    app,
-    resources={r"/*": {"origins": "*"}},
-    supports_credentials=True
-)
-
-
-    # swagger
-    from flask_swagger_ui import get_swaggerui_blueprint
-
+    app.config.from_object(Config)
+    
     SWAGGER_URL = '/docs'
     API_URL = '/static/swagger.json'
-
+    
     swaggerui_blueprint = get_swaggerui_blueprint(
-        SWAGGER_URL,
-        API_URL,
-        config={'app_name': "Book API"}
+        SWAGGER_URL,API_URL,config={'app_name':"API"}
     )
-    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+    app.register_blueprint(swaggerui_blueprint,url_prefix=SWAGGER_URL)
+
+    # Initialize extensions
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     @app.route("/")
     def index():
         return "Welcome to Just Hair Backend!"
 
-    # import models
+    # Import models so migrations can detect them
     from app import models
 
-    # register blueprints
+    # Register routes (Blueprints)
     from app.routes.routes import routes_bp
     app.register_blueprint(routes_bp)
+    from app.routes.reviews import  reviews_bp
+    app.register_blueprint(reviews_bp)
+
 
     return app
 
-# app = create_app()
+
+
+
+
+
+
--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from app import db, bcrypt
+from app import db
 from flask_login import UserMixin
 
 # ============================
